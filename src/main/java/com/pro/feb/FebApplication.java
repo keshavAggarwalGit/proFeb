e81b33bdbe84b061537a46068df7173dbfc73f96
keshav.aggarwal <keshav.aggarwal@83incs>
2020-12-06 00:55:36 +0530

    ini

---
+++ src/main/java/com/pro/feb/FebApplication.java
@@ -0,0 +1,13 @@
+package com.pro.feb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/feb/FebApplicationTests.java
@@ -0,0 +1,11 @@
+package com.pro.feb;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FebApplicationTests {
+
+
+
+}
